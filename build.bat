5dc69d805dee2c0d3ab58db50f8019e8dd3eb549
Steven Gann <sgann2012@fau.edu>
2020-07-17 14:13:41 -0700

    added artifacts

--- build.bat
+++ build.bat
@@ -13,16 +13,16 @@ cd ..\..\
 ECHO Archive Debug Build
 cd Source\HelloWorld\HelloWorld\bin\Debug\netcoreapp3.1\
 "C:\Program Files\7-Zip\7z.exe" a -tzip -r  Debug.zip *.*
-mkdir ..\..\..\..\..\Builds\
-move *.zip ..\..\..\..\..\Builds\
+mkdir ..\..\..\..\..\..\Builds\
+move *.zip ..\..\..\..\..\..\Builds\
 cd ..\..\..\..\..\..\
 @RD /S /Q Source\HelloWorld\HelloWorld\bin\Debug
 
 ECHO Archive Release Build
 cd Source\HelloWorld\HelloWorld\bin\Release\netcoreapp3.1\
 "C:\Program Files\7-Zip\7z.exe" a -tzip -r  Release.zip *.*
-mkdir ..\..\..\..\..\Builds\
-move *.zip ..\..\..\..\..\Builds\
+mkdir ..\..\..\..\..\..\Builds\
+move *.zip ..\..\..\..\..\..\Builds\
 cd ..\..\..\..\..\..\
 @RD /S /Q Source\HelloWorld\HelloWorld\bin\Release
 
